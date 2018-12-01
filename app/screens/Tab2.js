280581722af45dced643b606263e6122294c5b35
Charlie Harding <xsanda@me.com>
2018-12-01 13:07:33 +0100

    Fix tab 2 styles

--- app/screens/Tab2.js
+++ app/screens/Tab2.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
+import Styles from '../styles';
 
 export default class Tab2 extends Component {
   render() {
