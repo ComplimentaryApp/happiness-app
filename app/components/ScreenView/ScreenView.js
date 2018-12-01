8cd29c09f5463ecbca1f93c88e2b1a41568076d9
Charlie Harding <xsanda@me.com>
2018-12-01 14:42:53 +0100

    Fix Title bar again on Android

--- app/components/ScreenView/ScreenView.js
+++ app/components/ScreenView/ScreenView.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 import { SafeAreaView } from 'react-navigation';
-import { Platform } from 'react-native';
+import { Platform, Text } from 'react-native';
 
 export default class ScreenView extends Component {
   render() {
     const { style, ...props } = this.props;
     const styles = { paddingTop: Platform.OS === 'android' ? 25 : 0, ...style };
-    return (
-      <SafeAreaView styles={styles} {...props}>
-        {props.children}
-      </SafeAreaView>
-    );
+    return <SafeAreaView style={styles}>{props.children}</SafeAreaView>;
   }
 }
