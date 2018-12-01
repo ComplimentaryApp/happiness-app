a35c089910cf8f58bc60b270a38f17522a8d7a26
Charlie Harding <xsanda@me.com>
2018-12-01 14:47:29 +0100

    Fix PropTypes warning on ScreenView

--- app/components/ScreenView/ScreenView.js
+++ app/components/ScreenView/ScreenView.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import { SafeAreaView } from 'react-navigation';
-import { Platform, Text } from 'react-native';
+import { Platform } from 'react-native';
+import PropTypes from 'prop-types';
 
 export default class ScreenView extends Component {
+  static propTypes = {
+    style: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+  };
+
   render() {
     const { style, ...props } = this.props;
     const styles = { paddingTop: Platform.OS === 'android' ? 25 : 0, ...style };
